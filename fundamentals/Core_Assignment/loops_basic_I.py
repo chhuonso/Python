53ef71a9b8da31e79a4ca1abd350deaec820c7e7
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-07-04 19:13:24 -0700

    fixed line 6 added 5  step

--- fundamentals/Core_Assignment/loops_basic_I.py
+++ fundamentals/Core_Assignment/loops_basic_I.py
@@ -3,7 +3,7 @@ for i in range(151):
     print(i)
 
 #Multiples of Five - Print all the multiples of 5 from 5 to 1,000
-for i in range(5,1001):
+for i in range(5,1001, 5):
     print(i)
 
 #Counting, the Dojo Way - Print integers 1 to 100. 
